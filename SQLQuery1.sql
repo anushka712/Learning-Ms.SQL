55d029461e85c76a2b63c13a48a7958c1ec2c1b8
anushka712 <103297378+anushka712@users.noreply.github.com>
2023-03-28 20:35:22 +0545

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,21 @@
+--To create a new table which contain some attributes
+--syntax: create table table-name
+create table employee
+(
+emp_id int,
+name varchar(50),
+address varchar(50),
+phone_no numeric
+);
+
+select *from employee
+--(select *from "Table-name") is used to view all the data from a table
+
+drop table employee
+--It is used to remove existing database object
+  
+
+
+
+
+
