2b516c2431f244282206a8d2cafa0ed149f6a742
anushka712 <103297378+anushka712@users.noreply.github.com>
2023-04-03 18:54:51 +0545

    Add files via upload

---
+++ Basics/insertintotable.sql
@@ -0,0 +1,18 @@
+--Inserting the values into table using the key (insert into table-name)
+create table empl
+(
+emp_id int,
+emp_name varchar(25),
+phone_no numeric,
+address varchar(50)
+);
+ 
+select *from empl
+
+drop table empl
+
+insert into empl
+values(1,'Ankita','9856065280','Salyan'),
+	  (2,'Nikita','9838967451','Bajang'),
+	  (3,'Shristi','981223096','Baitadi'),
+	  (4,'Prerana','945628810','Palpa');
